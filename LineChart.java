0f524975d07605ecaa3ed33b944c081fc880e9a4
MikolajMroz <01171255@pw.edu.pl>
2023-01-21 18:49:44 +0100

    poprawilem wyglad

--- LineChart.java
+++ LineChart.java
@@ -1,6 +1,7 @@
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 import java.awt.*;
