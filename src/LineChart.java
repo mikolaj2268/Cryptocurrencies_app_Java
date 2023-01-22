6037ee456e1f47d458bc958af7be3580400e459b
wojo501 <108939761+wojo501@users.noreply.github.com>
2023-01-22 21:13:56 +0100

    zmieniono wykres - pokazywanie dat

--- src/LineChart.java
+++ src/LineChart.java
@@ -4,11 +4,16 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.Axis;
 import org.jfree.chart.axis.CategoryAxis;
 import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.Plot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
 
 import java.awt.*;
 import java.text.DateFormat;
@@ -28,24 +33,25 @@ public class LineChart {
         // Create dataset
         this.dates = dates;
         this.prices = prices;
-        DefaultCategoryDataset dataset = createDataset(name, startDate, endDate);
+        TimeSeriesCollection dataset = createDataset(name, startDate, endDate);
         // Create chart
-        JFreeChart chart = ChartFactory.createLineChart(
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
                 name.toUpperCase(), // Chart title
                 "Date", // X-Axis Label
                 "USD$", // Y-Axis Label
-                dataset
+                dataset,
+                false,
+                true,
+                true
         );
         chart.setBackgroundPaint(Color.black);
         chart.getPlot().setBackgroundPaint(Color.black);
         chart.getTitle().setPaint(Color.white);
-        Plot plot = chart.getPlot();
-        setAxisFontColor(((CategoryPlot) plot).getDomainAxis(), Color.white);
-        setAxisFontColor(((CategoryPlot) plot).getRangeAxis(), Color.white);
-        chart.removeLegend();
-        CategoryAxis domainAxis = ((CategoryPlot) plot).getDomainAxis();
-//        domainAxis.setCategoryLabelPositions(
-//                CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 4.0));
+        XYPlot plot = (XYPlot) chart.getPlot();
+        DateAxis axis = (DateAxis) plot.getDomainAxis();
+        setAxisFontColor(axis, Color.white);
+        Axis axis1 = plot.getRangeAxis();
+        setAxisFontColor(axis1, Color.white);
 
         this.panel = new ChartPanel(chart);
     }
@@ -53,9 +59,10 @@ public class LineChart {
     private void setAxisFontColor(Axis axis, Color fontColour) {
             axis.setLabelPaint(fontColour);
             axis.setTickLabelPaint(fontColour);
+
     }
 
-    private DefaultCategoryDataset createDataset(String name, String startDate, String endDate){
+    private TimeSeriesCollection createDataset(String name, String startDate, String endDate){
         DateFormat sdformat = new SimpleDateFormat("yyyy-MM-dd");
 
         Date startDateP = null;
@@ -69,13 +76,14 @@ public class LineChart {
 
         String series1 = name;
 
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        TimeSeries series = new TimeSeries(name);
         for (int i = 0; i < dates.size(); i++) {
-
             if(dates.get(i).compareTo(startDateP)>0 & endDateP.compareTo(dates.get(i))>0) {
-                dataset.addValue(Float.valueOf(prices.get(i)), series1, dates.get(i));
+                series.add(new Day(dates.get(i)),Float.valueOf(prices.get(i)));
             }
         }
+        dataset.addSeries(series);
         return dataset;
     }
 
