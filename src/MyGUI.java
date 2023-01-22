5dc4ad7318a9eb4e758f99c4e90c098923e94b99
MikolajMroz <01171255@pw.edu.pl>
2023-01-22 22:52:37 +0100

    wieksze przyciski

--- src/MyGUI.java
+++ src/MyGUI.java
@@ -1,25 +1,23 @@
 import com.formdev.flatlaf.FlatDarkLaf;
-import com.formdev.flatlaf.FlatLightLaf;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
-import javax.swing.JButton;
-import javax.swing.JFrame;
 
 public class MyGUI {
     public static void main(String[] args) {
-        try {UIManager.setLookAndFeel( new FlatLightLaf() );}
-        catch( Exception e ) { e.printStackTrace();}
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         String currCrypto = "bitcoin";
         FileReaderJ readerJ = new FileReaderJ();
-        readerJ.readFile("response.json");
+        readerJ.readFile("./src/API/final_response.json");
         HashMap<String, ArrayList<Date>> graphDates = readerJ.getGraphDates();
         HashMap<String, ArrayList<String>> graphPrices = readerJ.getGraphPrices();
         System.out.println(graphPrices.get(currCrypto));
@@ -27,7 +25,7 @@ public class MyGUI {
 
         // Create the frame
         JFrame frame = new JFrame("CryptoCurrency.com");
-        frame.setLayout(new FlowLayout());
+        frame.setLayout(new BorderLayout());
         frame.setSize(9000, 8000);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -35,31 +33,48 @@ public class MyGUI {
         JPanel leftPanel = new JPanel();
         leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
         StringButton s1 = new StringButton("Bitcoin");
+
+        s1.setBackground(Color.BLACK);
+        s1.setForeground(Color.WHITE);
+        s1.setFont(new Font("Arial", Font.BOLD, 40));
         leftPanel.add(s1);
         StringButton s2 = new StringButton("Ethereum");
+        s2.setBackground(Color.BLACK);
+        s2.setForeground(Color.WHITE);
+        s2.setFont(new Font("Arial", Font.BOLD, 40));
         leftPanel.add(s2);
         StringButton s3 = new StringButton("Tether");
+        s3.setBackground(Color.BLACK);
+        s3.setForeground(Color.WHITE);
+        s3.setFont(new Font("Arial", Font.BOLD, 40));
         leftPanel.add(s3);
         StringButton s4 = new StringButton("Solana");
+        s4.setBackground(Color.BLACK);
+        s4.setForeground(Color.WHITE);
+        s4.setFont(new Font("Arial", Font.BOLD, 40));
         leftPanel.add(s4);
-        DateButton date1 = new DateButton("From date:");
+        DateButton date1 = new DateButton("");
+        date1.setBackground(Color.BLACK);
+        date1.setForeground(Color.WHITE);
+        date1.setFont(new Font("Arial", Font.BOLD, 40));
         leftPanel.add(date1);
-        DateButton date2 = new DateButton("To date:");
+        DateButton date2 = new DateButton("");
+        date2.setBackground(Color.BLACK);
+        date2.setForeground(Color.WHITE);
+        date2.setFont(new Font("Arial", Font.BOLD, 40));
         leftPanel.add(date2);
+
         // Create the right panel with the graph
         JPanel rightPanel = new JPanel();
-        // create graph
-
-
-        LineChart graph1 = new LineChart("2002-01-01", "2023-01-01", currCrypto, graphDates.get(currCrypto),graphPrices.get(currCrypto));
         rightPanel.setLayout(new BorderLayout());
+        LineChart graph1 = new LineChart("2002-01-01", "2023-01-01", currCrypto, graphDates.get(currCrypto),graphPrices.get(currCrypto));
         rightPanel.add(graph1.panel, BorderLayout.CENTER);
-        rightPanel.validate();
-
-
 
         // Create the "show" button
         JButton showButton = new JButton("Show");
+        showButton.setBackground(Color.RED);
+        showButton.setForeground(Color.WHITE);
+        showButton.setFont(new Font("Arial", Font.BOLD, 40));
         showButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -76,15 +91,13 @@ public class MyGUI {
             }
         });
 
+        leftPanel.add(showButton);
         // Add the panels and button to the frame
         frame.add(leftPanel, BorderLayout.WEST);
-        frame.add(rightPanel, BorderLayout.EAST);
-        frame.add(showButton, BorderLayout.SOUTH);
+        frame.add(rightPanel, BorderLayout.CENTER);
 
         // Pack and display the frame
         frame.pack();
         frame.setVisible(true);
-
-
     }
 }
